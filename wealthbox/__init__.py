9c2085a715a59984334ca4e0d9a5208b87eaa34b
spencerogden-dsam <67068943+spencerogden-dsam@users.noreply.github.com>
2024-02-20 20:33:10 -0500

    Update api request method to handle pagination

--- wealthbox/__init__.py
+++ wealthbox/__init__.py
@@ -8,20 +8,31 @@ class WealthBox(object):
         self.token = token
         self.base_url = f"https://api.crmworkspace.com/v1/"
     
-    def api_request(self, endpoint, params=None):
+   def api_request(self, endpoint, params=None):
         url = self.base_url + endpoint
-        if params is None: params = {}
-
-        params.setdefault('per_page','5000')
-
-        res = requests.get(url,
-            params = params,
-            headers={'ACCESS_TOKEN':self.token})
+        page = 1
+        total_pages = 9999999999
+        if params is None:
+            params = {}
         
-        try:
-            return res.json()
-        except JSONDecodeError:
-            return res.text
+        params.setdefault('per_page', '5000')
+        results = []
 
+        while page <= total_pages:
+            params['page'] = page
+            res = requests.get(url,
+                            params=params,
+                            headers={'ACCESS_TOKEN': self.token})
+            try:
+                res_json = res.json()
+                total_pages = res_json['meta']['total_pages']
+                results.extend(res_json[endpoint])
+                page += 1
+            except:# JSONDecodeError:
+                print(res.text)
+                return results
+
+        return results
+       
     def get_contacts(self, filters=None):
         return self.api_request('contacts',params=filters)

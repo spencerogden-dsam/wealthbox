2631de27e897fdf3adf59a0bcbce1b7e07d285fa
spencerogden-dsam <67068943+spencerogden-dsam@users.noreply.github.com>
2024-02-20 21:22:24 -0500

    Add additional methods for other API endpoints

--- wealthbox/__init__.py
+++ wealthbox/__init__.py
@@ -6,6 +6,7 @@ __version__ = '0.1.0'
 class WealthBox(object):
     def __init__(self, token=None):
         self.token = token
+        self.user_id = None
         self.base_url = f"https://api.crmworkspace.com/v1/"
     
     def api_request(self, endpoint, params=None):
@@ -25,3 +26,15 @@ class WealthBox(object):
 
     def get_contacts(self, filters=None):
         return self.api_request('contacts',params=filters)
+
+    def get_tasks(self, filters=None):
+        return self.api_request('tasks',params=filters)
+
+    def get_my_user_id(self):
+        self.user_id = self.api_request('me')['current_user']['id']
+        return self.user_id
+
+    def get_my_tasks(self):
+        if self.user_id is None:
+            self.get_my_user_id()
+        return self.get_tasks({'assigned_to':self.user_id})

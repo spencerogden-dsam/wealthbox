a224ff1a96aa4666e8b55dc98597eb52d6e83270
spencerogden-dsam <67068943+spencerogden-dsam@users.noreply.github.com>
2024-03-07 22:18:34 -0500

    Major updates to add methods for gathering tasks and workflows and associated comments. Also added a helper function to associate resources with the username of the creator.

--- wealthbox/__init__.py
+++ wealthbox/__init__.py
@@ -1,7 +1,9 @@
 from json import JSONDecodeError
 import requests
 
-__version__ = '0.3.2'
+import importlib.metadata
+
+__version__ = importlib.metadata.version("wealthbox")
 
 class WealthBox(object):
     def __init__(self, token=None):
@@ -64,20 +66,42 @@ class WealthBox(object):
     def get_contacts(self, filters=None):
         return self.api_request('contacts',params=filters)
 
-    def get_tasks(self, filters=None):
-        return self.api_request('tasks',params=filters)
+    def get_tasks(self, resource_id=None, resource_type='contact', assigned_to=None, completed=True,other_filters=None):
+        default_params = {
+            'resource_type' : 'contact',
+            'completed' : True,
+            }
+
+        called_params = {
+            'resource_id': resource_id,
+            'resource_type': resource_type,
+            'assigned_to': assigned_to,
+            'completed': completed
+            }
+        other_filters = {} if other_filters is None else other_filters
+        # Merge dicts and remove keys with None values
+        called_params = {k: v for k, v in called_params.items() if v is not None}
+
+        return self.api_request('tasks',params={**default_params, **called_params,**other_filters})
     
     def get_workflows(self, resource_id=None, resource_type=None, status=None):
-        params = {}
-        if resource_id:
-            params['resource_id'] = resource_id
-        if resource_type:
-            params['resource_type'] = resource_type
-        if status:
-            params['status'] = status
-        return self.api_request('workflows',params=params)
+        default_params = {
+            'resource_type' : 'contact',
+            'status' : 'active',
+        }
+        called_params = {
+            'resource_id': resource_id,
+            'resource_type': resource_type,
+            'status': status,
+        }
+        # Merge dicts and remove keys with None values
+        called_params = {k: v for k, v in called_params.items() if v is not None}
+        
+        p = {**default_params, **called_params}
+        print(f'Calling workflows with params: {p}')
+        return self.api_request('workflows',params={**default_params, **called_params})
     
-    def get_events(self, resource_id=None, resource_type=None):
+    def get_events(self, resource_id=None, resource_type='contact'):
         params = {}
         if resource_id:
             params['resource_id'] = resource_id
@@ -110,7 +134,7 @@ class WealthBox(object):
             params['document_type'] = document_type
         return self.api_request('categories/tags',params=params)
     
-    def get_comments(self, resource_id=None, resource_type=None):
+    def get_comments(self, resource_id, resource_type='status_update'):
         params = {}
         if resource_id:
             params['resource_id'] = resource_id
@@ -139,6 +163,71 @@ class WealthBox(object):
         # Update a contact in WealthBox with a given ID and field data
         #TODO: Add custom field update
         return self.api_put(f'contacts/{contact_id}',updates_dict)
+
+    def get_notes_with_comments(self,contact_id):
+        notes = self.get_notes(contact_id)
+        for note in notes:
+            note['comments'] = self.get_comments(note['id'])
+        return notes
+
+    def get_events_with_comments(self,contact_id):
+        events = self.get_events(contact_id)
+        for event in events:
+            event['comments'] = self.get_comments(event['id'], resource_type='event')
+        return events
+
+    def get_tasks_with_comments(self,contact_id):
+        tasks = self.get_tasks(contact_id)
+        for task in tasks:
+            task['comments'] = self.get_comments(task['id'], resource_type='task')
+        return tasks
+
+    def get_workflows_with_comments(self,contact_id):
+        # First get all workflows, completed, active and scheduled
+        workflows = (
+            self.get_workflows(contact_id, status='active') + 
+            self.get_workflows(contact_id, status='completed') + 
+            self.get_workflows(contact_id, status='scheduled'))
+            
+        for wf in workflows:
+            for step in wf['workflow_steps']:
+                step['comments'] = self.get_comments(step['id'], resource_type='WorkflowStep')
+        return workflows
+
+    def get_users(self):
+        return self.api_request('users')
         
-        
-        
+    def make_user_map(self,method="full"):
+        user_list = self.get_users()
+        if method == "full":
+            user_dict = {user['id']:f'{user["id"]}; {user["name"]}; {user["email"]}' for user in user_list}
+        elif method == "name":
+            user_dict = {user['id']:user['name'] for user in user_list}
+        elif method == "first_name":
+            user_dict = {user['id']:user['name'].split(' ')[0] for user in user_list}
+        elif method == "email":
+            user_dict = {user['id']:user['email'] for user in user_list}
+        else:
+            raise Exception("method must be one of 'full', 'name', 'first_name', or 'email'")
+
+        return user_dict
+
+    def enhance_user_info(self,wb_data,method="full"):
+        """ Walk through a stucture of data from the API (list of dicts, dict of dicts,etc)
+        and replace the 'creator' field with information about the creator"""
+        if type(method) == dict:
+            user_map = method
+        else:
+            user_map = self.make_user_map(method)
+
+        # if wb_data is not a dict or list, just return it
+        if type(wb_data) not in [dict,list]:
+            return wb_data
+        if type(wb_data) == dict:
+            if 'creator' in wb_data:
+                wb_data['creator'] = user_map.get(wb_data['creator'],wb_data['creator']) 
+            if 'assigned_to' in wb_data:
+                wb_data['assigned_to'] = user_map.get(wb_data['assigned_to'],wb_data['assigned_to']) 
+            return {k:self.enhance_user_info(v,user_map) for k,v in wb_data.items()}
+        if type(wb_data) == list:
+            return [self.enhance_user_info(d,user_map) for d in wb_data]
